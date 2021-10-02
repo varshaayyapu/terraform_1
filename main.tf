98298eb288c09632182f5e49a5dda75dd986602e
varshaayyapu <89987154+varshaayyapu@users.noreply.github.com>
2021-10-01 20:55:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region     = "ca-central-1"
+  access_key = "AKIAZDRQUJRIOLVOJ35X"
+  secret_key = "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"
+}
 resource "aws_s3_bucket" "b" {
   bucket = "mybucket"
   acl    = "private"
